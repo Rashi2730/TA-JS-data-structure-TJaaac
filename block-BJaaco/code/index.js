ee87a613f2154552ae38e253192d259b4ae5733e
Rashi Israni <rashiisrani97@gmail.com>
2022-01-11 09:18:36 +0530

    resrh

--- block-BJaaco/code/index.js
+++ block-BJaaco/code/index.js
@@ -3,11 +3,40 @@ let numbers = [6, 8, 10, 12, 43, 56, 98];
 let userIds = [1230, 234, 1278, 984, 763, 900];
 
 // 1. Add all the values of numbers and userIds array into the new newly created array named `collection`
+let collection = [];
+for(let number of numbers){
+  collection.push(number);
+}
+for(let userId of userIds) {
+  collection.push(userId);
+}
 
 // 2. Add all the even numbers from both arrays numbers and userIds into a newly created array named `evenCollection`
+let evenCollection =[];
+for(let number of numbers){
+  if(number % 2 === 0) {
+    evenCollection.push(number);
+  }
+}
+for(let userId of userIds) {
+  if(userId % 2 === 0) {
+    evenCollection.push(userId);
+    }
+  }
+
 
 // 3. Add all the odd numbers from both arrays numbers and userIds into a newly created array named `oddCollection`
-
+let oddCollection =[];
+for(let number of numbers){
+  if(number % 2 !== 0) {
+    oddCollection.push(number);
+  }
+}
+for(let userId of userIds) {
+  if(userId % 2 !== 0) {
+    oddCollection.push(userId);
+    }
+  }
 /*
   @param means parameter
 
@@ -23,17 +52,21 @@ let userIds = [1230, 234, 1278, 984, 763, 900];
     times(0); // []
     times(5); // ['test', 'test', 'test', 'test', 'test']
 */
-
-function times() {
+let arrayTimes = [];
+function times( a, b) {
   // Your code
+arrayTimes.push(a);
+arrayTimes.push(b);
+return arrayTimes ;
 }
+
 
 // Uncomment the code below and test the output
 
-// console.log(times(5, 'c')); // ['c', 'c', 'c', 'c', 'c']
-// console.log(times(2, 'a')); // ['a', 'a']
-// console.log(times(0)); // []
-// console.log(times(5)); // ['test', 'test', 'test', 'test', 'test']
+console.log(times(5, 'c')); // ['c', 'c', 'c', 'c', 'c']
+//console.log(times(2, 'a')); // ['a', 'a']
+//console.log(times(0)); // []
+//console.log(times(5)); // ['test', 'test', 'test', 'test', 'test']
 
 /*
 
